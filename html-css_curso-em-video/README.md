29a639280e17549798b681ecf86c42ad3ca19248
LucivaldoDev <lucivaldo.jr@dobemtecnologia.com>
2025-02-24 12:55:27 -0300

    Initial commit

---
+++ html-css_curso-em-video/README.md
@@ -0,0 +1,2 @@
+# curso-em-video_html-css
+Exercicios feitos durante o cursop de HTML e CSS do Curso Em Video pelo professor Gustavo Guanabara
---
+++ javascript_curso-em-video/Módulo D - Condições em JavaScript/aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80
+console.log(`A velocidade de um carro é ${vel}km/h`)
+if (vel > 60) { //condição simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre com segurança`)
